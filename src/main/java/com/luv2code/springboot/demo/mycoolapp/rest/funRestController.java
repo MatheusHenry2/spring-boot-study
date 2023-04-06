4783a217960503adc9d3cf56f7fe77d072f18bb7
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2023-04-06 11:11:52 -0300

    Update funRestController.java

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/funRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/funRestController.java
@@ -9,7 +9,20 @@ public class funRestController {
     // expose "/" that return "Hello world"
 
     @GetMapping("/")
-    public String sayHello(){
+    public String sayHello() {
         return "hello world!";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "run a hard 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
     }
 }
